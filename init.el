50933eabc84640ebdc48c456be3a0f86e1b2541d
Sean Wall <spw@amazon.com>
2024-11-21 19:59:22 -0500

    feat: auto install and use treesitter grammars

--- init.el
+++ init.el
@@ -86,6 +86,13 @@
   :ensure t
   :config (which-key-mode))
 
+(use-package treesit-auto
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
+
 (use-package yasnippet
   :ensure t
   :config
