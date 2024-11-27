b56dc7084bc4e55069e1af42f76d832f7cf341c3
Sean Wall <spw@amazon.com>
2024-11-26 19:29:01 -0500

    feat: add typescript lsp support
    Needed to: brew install typescript-language-server

--- init.el
+++ init.el
@@ -524,6 +524,7 @@
   (setq lsp-inlay-hint-enable t)
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
          (rustic-mode . lsp)
+         (typescript-ts-mode . lsp)
          (python-mode . lsp)
          ;; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
