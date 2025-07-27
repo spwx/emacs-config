f0dd43306fdb83d9e52cdbd94b1136d8820090ca
Sean Wall <sean@wall.ninja>
2025-07-27 19:25:04 -0400

    feat: move custom vars to separate file

--- init.el
+++ init.el
@@ -149,26 +149,5 @@
 ;;
 ;; Custom Variable Configuration
 ;;
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("/Users/spw/org/logs/2025/07-July/00-july_tasks.org"
-     "/Users/spw/Documents/org/logs/2025/07-July/20250726.org") nil nil "Customized with use-package org")
- '(package-selected-packages nil)
- '(package-vc-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(evil-goggles-change-face ((t (:inherit diff-removed))))
- '(evil-goggles-delete-face ((t (:inherit diff-removed))))
- '(evil-goggles-paste-face ((t (:inherit diff-added))))
- '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
- '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
- '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
- '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
