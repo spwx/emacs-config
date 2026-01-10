971b37be6c2884d5657006b74388c89cfd441aa2
Sean Wall <spw@amazon.com>
2026-01-10 16:48:32 -0500

    feat: sync package-selected-packages for package-autoremove

--- use-package-config.el
+++ use-package-config.el
@@ -16,3 +16,12 @@
 
 ;; Auto install packages
 (setopt use-package-always-ensure t)
+
+;; Keep package-selected-packages in sync for package-autoremove
+(defun my/use-package-ensure-advice (name &rest _)
+  "Add NAME to `package-selected-packages' when use-package ensures it."
+  (when (and (not (package-built-in-p name))
+             (not (memq name package-selected-packages)))
+    (customize-save-variable 'package-selected-packages
+                             (cons name package-selected-packages))))
+(advice-add 'use-package-ensure-elpa :after #'my/use-package-ensure-advice)
